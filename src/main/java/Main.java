232d84cc90533c97a9fdf6f11970f3b77b72b159
Nikodem Adamczyk <adamczyknikus@gmail.com>
2024-11-08 12:52:17 +0100

    Changes in main branch - changing to economic cars

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,9 +3,9 @@ public class Main {
     public static void main(String[] args) {
         Factory factory = new Factory();
 
-        Car car1 = new Car("red", "BMW", 5);
-        Car car2 = new Car("blue", "Audi", 3);
-        Car car3 = new Car("blue", "VW", 3);
+        Car car1 = new Car("green", "Honda", 5);
+        Car car2 = new Car("red", "Kia", 3);
+        Car car3 = new Car("black", "Opel", 3);
 
         factory.buildCar(car1);
         factory.buildCar(car2);
