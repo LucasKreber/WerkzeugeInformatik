ee0a4bb73e974f71406a4dbd12e193e3024e3fa8
aptinstallpython3-pip <malaria367@gmail.com>
2025-10-23 17:38:18 +0200

    Update Main.java

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,11 +3,9 @@ public class Main {
     public static void main(String[] args) {
         Factory factory = new Factory();
 
-        Car car11 = new Car("red", "BMW", 5);
         Car car2 = new Car("blue", "Audi", 3);
         Car car3 = new Car("blue", "VW", 3);
 
-        factory.buildCar(car1);
         factory.buildCar(car2);
     }
 }
